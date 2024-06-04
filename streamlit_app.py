930db09db780ed922006e6fb628ada3f4027050f
Danielalj00 <162068890+Danielalj00@users.noreply.github.com>
2024-06-04 07:09:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,16 @@
 import streamlit as st
-import deepl
-import os
-from dotenv import load_dotenv
-
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
-
-# Configurar la API key de DeepL desde la variable de entorno
-deepl_api_key = os.getenv('DEEPL_API_KEY')
-translator = deepl.Translator(deepl_api_key)
+from translate import Translator
 
 st.title('Traducción Automática Multilingüe')
 
 # Diccionario de modelos para traducción
 model_dict = {
-    "Español a Inglés": ("ES", "EN-US"),
-    "Inglés a Español": ("EN", "ES"),
-    "Francés a Inglés": ("FR", "EN-US"),
-    "Inglés a Francés": ("EN", "FR"),
-    "Alemán a Inglés": ("DE", "EN-US"),
-    "Inglés a Alemán": ("EN", "DE"),
+    "Español a Inglés": ("es", "en"),
+    "Inglés a Español": ("en", "es"),
+    "Francés a Inglés": ("fr", "en"),
+    "Inglés a Francés": ("en", "fr"),
+    "Alemán a Inglés": ("de", "en"),
+    "Inglés a Alemán": ("en", "de"),
 }
 
 st.header('Seleccione el idioma de origen y destino')
@@ -28,14 +19,16 @@ option = st.selectbox('Selecciona la dirección de traducción', list(model_dict
 # Obtener los códigos de idioma
 src_lang, dest_lang = model_dict[option]
 
+# Crear el traductor
+translator = Translator(from_lang=src_lang, to_lang=dest_lang)
+
 # Entrada de texto del usuario
 user_input = st.text_area(f'Texto a traducir ({src_lang} a {dest_lang})')
 
 if st.button('Traducir'):
     if user_input:
         # Realizar la traducción
-        result = translator.translate_text(user_input, source_lang=src_lang, target_lang=dest_lang)
-        translated_text = result.text
+        translated_text = translator.translate(user_input)
 
         # Mostrar el texto traducido
         st.write('**Texto Traducido:**')
